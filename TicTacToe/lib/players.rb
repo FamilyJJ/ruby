e1920ff7832fb382f8a4aae5fe359495509b0162
dev-tools start <justinas.jonuska@vinted.com>
2024-09-10 19:21:41 +0300

    Add player input validation

--- TicTacToe/lib/players.rb
+++ TicTacToe/lib/players.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class Player
-  attr_accessor :player, :user_input, :player_symbol
+  attr_accessor :player, :player_symbol, :user_input
+
+  @@selected_options = []
+  POSSIBLE_OPTIONS = %w[1 2 3 4 5 6 7 8 9].freeze
 
   def initialize(player)
     @player = player
@@ -9,13 +12,23 @@ class Player
   end
 
   def move(board)
-    puts "Player #{@player} select number to place - #{@player_symbol}"
-    @user_input = gets.chomp.downcase
-    @board = board
-    @board.each do |array|
+
+    loop do
+      puts "Player #{@player} select number to place - #{@player_symbol}"
+      @user_input = gets.chomp.downcase
+      if !POSSIBLE_OPTIONS.include?(@user_input)
+        puts "\nWrong option Try again"
+      elsif @@selected_options.include?(@user_input)
+        puts "\nOption already taken. Try again."
+      else
+        @@selected_options.append(@user_input)
+        break
+      end
+    end
+
+    board.each do |array|
       array.map! { |option| option == "| #{@user_input} " ? "| #{@player_symbol} " : option }
     end
   end
 
 end
-
